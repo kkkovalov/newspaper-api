f3268c0965b81d83dca6f059f36ebb78e5205c7d
kkkovalov <vlad_kovalov@icloud.com>
2023-07-29 21:10:24 -0700

    Mid-way setup for separate branch of topic view implementation

--- newspaper/views/topic_views.py
+++ newspaper/views/topic_views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import exceptions as rest_exceptions
+from rest_framework import exceptions
 
 # local packages
 from newspaper.models import Topic
@@ -22,7 +22,7 @@ class TopicView(APIView):
         try:
             topics_list = Topic.objects.all()
         except:
-            raise rest_exceptions.NotFound
+            raise exceptions.NotFound
         serializer = TopicSerializer(topics_list, many=True)
         return Response(serializer.data, status=200)
     
@@ -37,19 +37,31 @@ class TopicView(APIView):
                 topic.save()
                 return Response(topic.data, status=200)
             else:
-                raise rest_exceptions.PermissionDenied
+                raise exceptions.PermissionDenied
         else:
-            raise rest_exceptions.AuthenticationFailed
+            raise exceptions.AuthenticationFailed
     
     def put(self, request, format=None):
         """
         Takes JSON object with keys (name, description). Returns updated object.
         """
-        return Response({'detail': 'put request response'}, status=200)
+        if request.user.is_authenticated:
+            if is_creator(request.user):
+                return Response({'detail': 'put request response'}, status=200)
+            else:
+                raise exceptions.PermissionDenied
+        else:
+            raise exceptions.AuthenticationFailed
     
     def delete(self, request, format=None):
         """
         Deletes the object and returns a confirmation message
         """
-        return Response({'detail': 'delete request response'}, status=200)
+        if request.user.is_authenticated:
+            if is_creator(request.user):
+                return Response({'detail': 'delete request response'}, status=200)
+            else:
+                raise exceptions.PermissionDenied
+        else:
+            raise exceptions.AuthenticationFailed
 
