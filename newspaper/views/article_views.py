ea2d0182680b85954d322e00f78326c302c088fd
kkkovalov <vlad_kovalov@icloud.com>
2023-07-29 21:13:24 -0700

    Initial setup for ArticleView class

--- newspaper/views/article_views.py
+++ newspaper/views/article_views.py
@@ -1,17 +1,11 @@
-from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
-from django.core import exceptions
-from django.contrib.auth import authenticate
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import user_passes_test
-from django.utils.text import slugify
-
 # rest_framework packages
 from rest_framework import serializers
+from rest_framework import exceptions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import exceptions as rest_exceptions
+from rest_framework.views import APIView
+
 
 # local packages
 from newspaper import models, forms
@@ -20,17 +14,30 @@ from newspaper.serializers import ArticleSerializer
 
 # Article views
 
+class AritcleView(APIView):
+    """
+    Article view class that handles all requests and configures authentication
+    """
+    def get(self, request, format=None):
+        pass
+    def post(self, request, format=None):
+        pass
+    def put(self, request, format=None):
+        pass
+    def delete(self, request, format=None):
+        pass
+
 @api_view(['GET'])
 def articlesView(request):
     if request.method == "GET":
         try:
             articles = models.Article.objects.all()
         except:
-            raise rest_exceptions.APIException
+            raise exceptions.APIException
         articles_serializer = ArticleSerializer(articles, many=True)
         return Response(articles_serializer.data, status=200)
     else:
-        raise rest_exceptions.MethodNotAllowed
+        raise exceptions.MethodNotAllowed
     
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -40,8 +47,8 @@ def singleArticleView(request, slug_name):
         try:
             article = models.Article.objects.get(slug_name=slug_name)
         except:
-            raise rest_exceptions.NotFound
+            raise exceptions.NotFound
         article_serializer = ArticleSerializer(article)
         return Response(article_serializer.data, status=200)
     else:
-        raise rest_exceptions.MethodNotAllowed
+        raise exceptions.MethodNotAllowed
