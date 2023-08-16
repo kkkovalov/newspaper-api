a30efe964b4b6bbf237c14b2b1a89e52e6fa6573
kkkovalov <vlad_kovalov@icloud.com>
2023-08-15 20:30:28 -0700

    updated

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-from dj_database_url import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
