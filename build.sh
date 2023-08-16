cdd9a1f6b75d7643ccd935254ad9ae7e2b589b78
kkkovalov <vlad_kovalov@icloud.com>
2023-08-15 22:02:02 -0700

    updated vercel.json file

--- build.sh
+++ build.sh
@@ -20,4 +20,4 @@ python manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
-# python manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
